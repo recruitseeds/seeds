72539e5c700b1ea2e9f615ebeaac5b69ba843070
alexwhitmore <heyimalexw@gmail.com>
2025-07-08 22:45:14 -0700

    Update dependencies and refactor editor components
    - Added `@floating-ui/react` dependency for improved positioning of UI elements.
    - Updated `@tiptap/extension-highlight` and other Tiptap extensions to version 2.25.0 for enhanced functionality.
    - Refactored the `BlockEditor` component to improve structure and remove unused imports.
    - Cleaned up the `left-sidebar` and `icons` components for better readability and maintainability.
    - Removed deprecated editor components and menus to streamline the codebase.
    - Enhanced the `use-editor` hook for better user experience in collaboration features.

--- editor/extensions/image-upload/view/image-upload.tsx
+++ editor/extensions/image-upload/view/image-upload.tsx
@@ -1,24 +1,13 @@
-import { Editor, NodeViewWrapper } from '@tiptap/react'
+import { type Editor, NodeViewWrapper } from '@tiptap/react'
 import { useCallback } from 'react'
 
 import { ImageUploader } from './image-uploader'
 
-export const ImageUpload = ({
-  getPos,
-  editor,
-}: {
-  getPos: () => number
-  editor: Editor
-}) => {
+export const ImageUpload = ({ getPos, editor }: { getPos: () => number; editor: Editor }) => {
   const onUpload = useCallback(
     (url: string) => {
       if (url) {
-        editor
-          .chain()
-          .setImageBlock({ src: url })
-          .deleteRange({ from: getPos(), to: getPos() })
-          .focus()
-          .run()
+        editor.chain().setImageBlock({ src: url }).deleteRange({ from: getPos(), to: getPos() }).focus().run()
       }
     },
     [getPos, editor]
